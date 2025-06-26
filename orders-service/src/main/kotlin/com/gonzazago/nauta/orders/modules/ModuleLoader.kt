1a3474769d16175d8c043d869afbbf17dd0274de
Gonzalo Sergio Zago <gonza.zago@gmail.com>
2025-06-26 09:50:48 -0300

    Update ModuleLoader.kt
    fix handler

--- orders-service/src/main/kotlin/com/gonzazago/nauta/orders/modules/ModuleLoader.kt
+++ orders-service/src/main/kotlin/com/gonzazago/nauta/orders/modules/ModuleLoader.kt
@@ -3,6 +3,9 @@ package com.gonzazago.nauta.orders.modules
 import com.gonzazago.nauta.orders.Server
 import com.gonzazago.nauta.orders.application.usecase.orders.CreateOrder
 import com.gonzazago.nauta.orders.delivery.rest.orders.OrderHandler
+import com.gonzazago.nauta.orders.domain.services.OrderService
+import com.gonzazago.nauta.orders.routes.OrderRouter
+import com.gonzazago.nauta.orders.routes.Router
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import io.vertx.core.Vertx
@@ -15,10 +18,13 @@ object ModuleLoader {
     private val appModule = module(createdAtStart = true) {
         single { vertxInstance }
         single<Config> { ConfigFactory.load() }
-        single { Server() }
-        single { OrderHandler(get()) }
-        single { CreateOrder(get()) }
+        single { Server(get()) }
+        single { OrderRouter(get()) }
+        single { Router(get(), get()) }
+        single { OrderService() }
 
+        handlers()
+        useCases()
         mapper()
     }
 
