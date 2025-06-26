bafa9c15edc8da9d085242c67f583c92c5a3f988
Gonzalo Sergio Zago <gonza.zago@gmail.com>
2025-06-26 09:59:54 -0300

    Update Server.kt

--- orders-service/src/main/kotlin/com/gonzazago/nauta/orders/Server.kt
+++ orders-service/src/main/kotlin/com/gonzazago/nauta/orders/Server.kt
@@ -1,6 +1,6 @@
 package com.gonzazago.nauta.orders
 
-import com.gonzazago.nauta.orders.routes.createRouter
+import com.gonzazago.nauta.orders.routes.Router
 import com.typesafe.config.Config
 import io.vertx.core.Vertx
 import io.vertx.core.impl.logging.LoggerFactory
@@ -9,14 +9,14 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import kotlin.getValue
 
-class Server : KoinComponent {
+class Server (    private val mainAppRouter:Router) : KoinComponent {
     private val config: Config by inject()
     private val vertx: Vertx by inject()
     private val log = LoggerFactory.getLogger(Server::class.java)
     fun start() {
         val portConfig: Int = config.getInt("server.port")
         val host = config.getString("server.host")
-        val router = createRouter(vertx)
+        val router = mainAppRouter.createRouter(vertx)
 
         vertx.createHttpServer()
             .requestHandler(router)
